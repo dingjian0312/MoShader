7ec3026594972da3a00a04314c97a5095b0b7505
dingjian0312 <466839201@qq.com>
2017-09-07 15:38:56 +0800

    add cull performance test

---
+++ Assets/Mo/Test/CullPerformanceTest/CloneObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour 
+{
+    public GameObject cube;
+    public int Count = 100;
+
+	// Use this for initialization
+	void Start () 
+    {
+		for (int i = 0; i < Count; ++i)
+        {
+            GameObject go = GameObject.Instantiate(cube);
+            go.transform.SetParent(transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
