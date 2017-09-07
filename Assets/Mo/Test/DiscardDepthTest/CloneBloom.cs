4ff991892a5f11a956ccfbd56f9050c7b44fc77c
dingjian0312 <466839201@qq.com>
2017-09-07 20:34:57 +0800

    add DiscardDepthTest

---
+++ Assets/Mo/Test/DiscardDepthTest/CloneBloom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneBloom : MonoBehaviour {
+    public int bloomCount = 20;
+	
+	void Start () 
+    {
+		for (int i = 0; i < bloomCount; ++i)
+        {
+            gameObject.AddComponent<Bloom>();
+        }
+	}
+	
+	
+	void Update () {
+		
+	}
+}
