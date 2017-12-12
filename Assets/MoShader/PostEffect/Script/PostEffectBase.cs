ee22ac4aba413f2fcc23071dadad416a27adf303
dingjian0312 <466839201@qq.com>
2017-12-12 14:47:46 +0800

    add Bloom EdgeDetection GaussianBlur

---
+++ Assets/MoShader/PostEffect/Script/PostEffectBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffectBase : MonoBehaviour
+{
+    protected Material CreateMaterial(Shader shader)
+    {
+        if (shader == null || !shader.isSupported)
+        {
+            Debug.LogError("Shader is error");
+            return null;
+        }
+
+        Material material = new Material(shader);
+        material.hideFlags = HideFlags.DontSave;
+        return material;
+    }
+}
